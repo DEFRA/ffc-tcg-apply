e82a7b20c3b59d61f545669d1c7474648662314c
John Watson <john.watson1@defra.gov.uk>
2023-08-22 09:29:39 +0100

    Add authentication

--- app/routes/confirmation.js
+++ app/routes/confirmation.js
@@ -1,10 +1,22 @@
-const { v4: uuidv4 } = require('uuid')
+const Joi = require('joi')
 const { GET } = require('../constants/http-verbs')
+const { USER } = require('../auth/scopes')
 
 module.exports = {
   method: GET,
   path: '/confirmation',
+  options: {
+    auth: { strategy: 'jwt', scope: [USER] },
+    validate: {
+      query: {
+        referenceNumber: Joi.string().required()
+      },
+      failAction: (request, h, error) => {
+        return h.redirect('/')
+      }
+    }
+  },
   handler: (request, h) => {
-    return h.view('confirmation', { referenceNumber: uuidv4() })
+    return h.view('confirmation', request.query)
   }
 }
