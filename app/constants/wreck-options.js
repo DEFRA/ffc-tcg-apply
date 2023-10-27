8dc5865a542d7bb886f6e08f6ba059074caf7bae
Simon Dunn <simondunn90@gmail.com>
2023-10-27 15:37:37 +0100

    setup temp auth

---
+++ app/constants/wreck-options.js
@@ -0,0 +1,13 @@
+const { TCG_TEMP_AUTH } = require('./cookies')
+const WRECK_OPTIONS = (payload) => {
+  return {
+    headers: {
+      authorization: `Bearer ${TCG_TEMP_AUTH}`
+    },
+    rejectUnauthorized: false,
+    json: true,
+    payload
+  }
+}
+
+module.exports = { WRECK_OPTIONS }
